d97d03555315f9e6c4e8b898917d5bbadabd82bd
timmoh-king <timothykariuki43@gmail.com>
2023-10-06 20:51:58 +0300

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 """Complex types - string and int/float to tuple"""
-from typing import Union
+from typing import Union, List
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: List[Union[int, float]]) -> tuple:
     """
         first element of the tuple is the string k
         The second element is the square of the int/float v
