76b24f0afa017488058f9a5d919571d6c855dd69
timmoh-king <timothykariuki43@gmail.com>
2023-10-06 16:36:00 +0300

    0-add.py

---
+++ 0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+    Write a type-annotated function add
+    that takes a float a and a float b as arguments
+    and returns their sum as a float.
+"""
+
+from typing import Callable, Iterator, Union, Optional
+
+
+def add(a: float, b: float) -> float:
+    return a + b
