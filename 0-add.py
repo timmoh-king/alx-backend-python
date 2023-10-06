a832d6ad48eae00b179e67560a036482c87eb126
timmoh-king <timothykariuki43@gmail.com>
2023-10-06 16:41:44 +0300

    0-add.py

--- 0-add.py
+++ 0-add.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
-
-"""
-    Write a type-annotated function add
-    that takes a float a and a float b as arguments
-    and returns their sum as a float.
-"""
+"""type annotated function"""
 
 
 def add(a: float, b: float) -> float:
