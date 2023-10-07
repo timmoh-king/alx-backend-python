fbf9d2c4adac86fbbccb8840231f3c21c2af65cc
timmoh-king <timothykariuki43@gmail.com>
2023-10-07 12:13:25 +0300

    9-annote.py

--- 0x00-python_variable_annotations/9-annote.py
+++ 0x00-python_variable_annotations/9-annote.py
@@ -7,6 +7,6 @@
 from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: Iterable[Sequence]): -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """return a tuple"""
     return [(i, len(i)) for i in lst]
