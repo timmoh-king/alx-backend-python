56600390ca1f7e7b9753e69d1444e55350d5a9f4
timmoh-king <timothykariuki43@gmail.com>
2023-10-10 06:02:19 +0300

    Remove duplicate values in a list

--- 0x00-python_variable_annotations/swap.py
+++ 0x00-python_variable_annotations/swap.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
-list = [6, 5, 7, 9, 13, 3]
-temp_list = []
+list = [6, 5, 5, 7, 9, 13, 3]
+max = list[0]
 
-for i in range(len(list)):
-    if list[i] > list[i + 1]:
-        list[i] = list[i + 1]
-        list[i + 1] = list[i]
-        print(list)
+for i in list:
+    if i > max:
+        max = i
+print(max)
+
+numbers = []
+
+for i in list:
+    if i not in numbers:
+        numbers.append(i)
+
+print(numbers)
